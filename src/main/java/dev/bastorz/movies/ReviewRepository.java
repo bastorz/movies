75ef3148d7f08099cf27b5c96ec5021ade3c977e
bastorz <damianalcalayy@gmail.com>
2023-06-04 22:18:33 +0100

    first commit

---
+++ src/main/java/dev/bastorz/movies/ReviewRepository.java
@@ -0,0 +1,9 @@
+package dev.bastorz.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
